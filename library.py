d982cbad9c16d2726b37b0a2d9b110f22e50314e
aqw3st <68729515+aqw3st@users.noreply.github.com>
2021-09-29 16:25:16 +0300

    Add files via upload

--- library.py
+++ library.py
@@ -1,16 +1,73 @@
-import flask
-from flask import Flask, render_template, request, redirect
-from model import malware_ner
-from flaskext.markdown import Markdown
-
-import urllib.request
-from bs4 import BeautifulSoup
-import PyPDF2
-from io import open 
 import re
-import requests
 
-####################NLP###########################
-import spacy
-from spacy import displacy
-from spacy.tokenizer import Tokenizer
+def find_CVE(text_data):
+	cve_pattern = 'CVE-\d{4}-\d{4,7}' 
+	cve_find = re.findall(cve_pattern, text_data)
+	return cve_find 
+
+def find_APT(text_data):
+	apt_pattern = 'APT[0-9]{1,2}'
+	apt_find = re.findall(apt_pattern, text_data)
+	return apt_find 	
+
+def find_HASH(text_data):
+	hash_pattern = '[a-fA-F\d]{32}'
+	hash_find = re.findall(hash_pattern, text_data)
+	return hash_find
+
+def find_URL(text_data):
+	url_pattern = '''(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?'''
+	url_find = re.findall(url_pattern, text_data)
+	return url_find
+
+def find_IP(text_data):
+	ip_pattern = '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|\[.])(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|\[.])(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|\[.])(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
+	ip_find = re.findall(ip_pattern, text_data)
+	ips = []
+	# convert list to string by element
+	for i in range(len(ip_find)):
+		ips.append(''.join(ip_find[i]))	
+	return ips
+
+def find_DOMAIN(text_data):
+	domain_pattern = '^(((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.)?(x--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})\.?$'
+	domain_find = re.findall(domain_pattern, text_data)
+	return domain_find
+
+def find_MALWARE(doc):	
+	malwares = []
+	for ents in doc.ents:
+		if ents.label_ == 'Malware':
+		    malwares.append(ents)
+	return malwares
+
+
+def highlight_html(cve_find, hash_find, malwares, domain_find, ips, apt_find, text_data):
+	
+	#highlights iocs on html page
+	mark = '<mark>{}</mark>'
+	for cve in cve_find:
+		if cve in text_data:
+		    text_data = text_data.replace(cve,mark.format(cve))
+
+	for hash in hash_find:
+		if hash in text_data:
+		    text_data = text_data.replace(hash,mark.format(hash))
+
+	for malware in malwares:
+		if str(malware) in text_data:
+		    text_data = text_data.replace(str(malware),mark.format(str(malware)))
+		
+	for domain in domain_find:
+		if domain in text_data:
+			text_data = text_data.replace(domain,mark.format(domain))
+
+	for ip in ips:
+		if ip in text_data:
+			text_data = text_data.replace(ip,mark.format(ip))
+
+	for apt in apt_find:
+		if apt in text_data:
+			text_data = text_data.replace(apt,mark.format(apt))
+
+	return text_data
--- main.py
+++ main.py
@@ -1,24 +1,33 @@
+#FLASK
 import library
-# import flask
-# from flask import Flask, render_template, request, redirect
-# from model import malware_ner
-# from flaskext.markdown import Markdown
+from library import *
+import flask
+from flask import Flask, render_template, request, redirect
+from model import malware_ner
+from flaskext.markdown import Markdown
 
-# import urllib.request
-# from bs4 import BeautifulSoup
-# import PyPDF2
-# from io import open 
-# import re
-# import requests
+# PARSING
+import urllib.request
+from bs4 import BeautifulSoup
+import PyPDF2
+import re
+import requests
+from spacypdfreader import pdf_reader
+import pdftotree
 
-# ####################NLP###########################
-# import spacy
+#NLP
+import spacy
 # from spacy import displacy
 # from spacy.tokenizer import Tokenizer
-nlp = spacy.load('model/malware_ner') #nlp model for detecting malware v.1
+nlp = spacy.load('model/Spacy Model v.777 02.09') #nlp model for detecting malware v.1
 
+#ANOTHER
+import os
+import sys
+# upgrade = '''<span id="7d1074629" class="_1Ivbd_21aL6" name="{}" style="pointer-events: all; cursor: pointer; z-index: 1; line-height: inherit; font-style: inherit; font-size: inherit; color: inherit; font-family: inherit; text-indent: initial; display: inline-block; word-break: break-word; vertical-align: middle; min-width: auto; position: relative; right: 6px; height: inherit; border-radius: 4px; border: 2px solid rgb(245, 166, 35); background-color: transparent;">{}</span>'''
 
-
+#OTHERS
+from io import open 
 
 # header for correct parsing html
 headers = {'User-Agent': 
@@ -31,40 +40,43 @@ headers = {'User-Agent':
 app = Flask(__name__)
 Markdown(app)
 
+
+#MAIN PAGE
 @app.route('/', methods=['POST','GET'])
 
 def hello():
 	return render_template('index.html')
 
 
-@app.route('/result', methods=['POST','GET'])
-
-def extract():
-	if request.method == 'POST':
-		rawtext = request.form['text_ner']	
-		doc = nlp(rawtext)
-		html = displacy.render(doc,style='ent')
-		result = html
-	return render_template('result.html', rawtext=rawtext, result = result)
-
-
+#RENDER WEB PAGE AND DETECT ENTYTIES IN BROWSER LOCALLY
 @app.route('/page', methods=['POST','GET'])
+
 def render_url():
-
-	mark = '<mark>{}</mark>' # how highlights words in html page
-
+	# mark = '<mark>{}</mark>'
 	if request.method == 'POST':
-		url = request.form['get_url'] #get url
-		text_data = requests.get(url, headers = headers).text # get raw html code
-		soup = BeautifulSoup(text_data,'html') # get content from raw html
-		text_data = str(soup) # 
-		doc = nlp(soup.text)
-		for entity in list(doc.ents):
-			if str(entity) in text_data.split():
-				text_data = text_data.replace(str(entity), mark.format(entity))
-		html_code = BeautifulSoup(text_data,'html')
+		url = request.form['get_url_please'] #get url
+		raw_html = requests.get(url, headers = headers,verify=False).text # get raw html code
+		soup = BeautifulSoup(raw_html,features='html.parser') # get content from raw html
+		text_data = str(soup) 
+		doc = nlp(soup.text)	
 
-	return render_template('webpage.html',html_code = html_code)
+		#FIND IOCs
+		domain_find = find_DOMAIN(text_data)
+		cve_find = find_CVE(text_data)
+		apt_find = find_APT(text_data)
+		hash_find = find_HASH(text_data)
+		url_find = find_URL(text_data)
+		ips = find_IP(text_data)
+		malwares = find_MALWARE(doc)
+
+
+		#Highlights found iocs on html page
+		data = highlight_html(cve_find, hash_find, malwares, domain_find, ips, apt_find, text_data)
+		html_text = BeautifulSoup(data,features='html.parser')
+		
+	return render_template('webpage.html',html_text = html_text)
+
+
 
 
 @app.route('/try_render')
@@ -75,17 +87,9 @@ def render_try():
 
 
 
-@app.route('/pdf', methods=['POST','GET'])
-def render_pdf():
-	if request.method == 'POST':
-		pdf = request.files['file']
 
-		# pdf = pdf.read()
-		# pdfFileObj = open(pdf, 'rb',encoding='latin1') 
-		# pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
-		# pageObj = pdfReader.getPage(0) 
-		# result = pageObj.extractText()
-	return render_template('pdf_open.html',result = result)
+
+
 
 
 
